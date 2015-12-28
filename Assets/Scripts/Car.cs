8de82dbff1facab28ba8e7cbc26838e8afccf17f
Graham Burgsma <graham.burgsma@gmail.com>
2015-12-28 15:46:23 -0500

    Car is now faster and drives better (using forces). Improved suspension and steering. Steering still needs improvement at high speeds.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -18,15 +18,21 @@ namespace LittleRocketLeague {
 
 		[SerializeField] Wheel[] wheels = new Wheel[0];
 		[SerializeField] float enginePower = 0, turnPower = 0, brakePower = 0, jumpForce = 40000, nitroForce = 15000;
-		Rigidbody rB;
+		new Rigidbody rigidBody;
+		new ConstantForce constantForce;
+
+		private float torque = 0, turnSpeed = 0;
 
 		// Use this for initialization
 		void Start() {
-			rB = GetComponent<Rigidbody>();
+			rigidBody = GetComponent<Rigidbody>();
+			constantForce = GetComponent<ConstantForce>();
 		}
 	 
 		//Visual updates - every frame
 		void Update() {
+			int numWheelsGrounded = 0;
+
 			//Reset Car
 			if (Input.GetKeyDown("r")) {
 				transform.position = new Vector3(transform.position.x, 10, transform.position.z);
@@ -34,53 +40,56 @@ namespace LittleRocketLeague {
 			}
 
 			foreach (Wheel wheel in wheels) {
+				if (wheel.wheelCollider.isGrounded)
+					numWheelsGrounded++;
+				
 				wheel.wheelTransform.Rotate(Vector3.right * wheel.wheelCollider.rpm / 60 * 360 * Time.deltaTime);
 
 				if (wheel.steer) {
 					wheel.wheelTransform.localEulerAngles = new Vector3(wheel.wheelTransform.localEulerAngles.x, wheel.wheelCollider.steerAngle - wheel.wheelTransform.localEulerAngles.z, wheel.wheelTransform.localEulerAngles.z);
 				}
 			}
+				
+			//Forces
+			if (numWheelsGrounded > 0) {
+				constantForce.relativeForce = new Vector3(0, -2500, torque * 10);
+				constantForce.relativeTorque = Vector3.up * turnSpeed * 4000;
+			} else {
+				constantForce.relativeForce = Vector3.down * 2500;
+				constantForce.relativeTorque = Vector3.zero;
+			}
+
+			if (Input.GetKeyDown(KeyCode.J)) {
+				if (numWheelsGrounded > 0) {
+					rigidBody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+				}
+			}
+
+			if (Input.GetKeyDown(KeyCode.N)) {
+				rigidBody.AddForce(transform.forward * nitroForce, ForceMode.Impulse);
+			}
 		}
 
 		//Physics updates - more frequent
 		void FixedUpdate() {
-			float torque = Input.GetAxis("Vertical") * enginePower;
-			float turnSpeed = Input.GetAxis("Horizontal") * turnPower * 2;
+			torque = Input.GetAxis("Vertical") * enginePower;
+			turnSpeed = Input.GetAxis("Horizontal") * turnPower * 2;
 			float brake = Input.GetAxis("Jump") * brakePower;
-			int jump = 0;
+			int numWheelsGrounded = 0;
 
 			foreach (Wheel wheel in wheels) {
 				if (wheel.wheelCollider.isGrounded)
-					jump++;
+					numWheelsGrounded++;
 				
 				if (wheel.handbrake)
 					wheel.wheelCollider.brakeTorque = brake;
 
 				if (wheel.power)
-					wheel.wheelCollider.motorTorque = torque;
+					wheel.wheelCollider.motorTorque = 0; //torque;
 
 				if (wheel.steer)
 					wheel.wheelCollider.steerAngle = turnSpeed;
 			}
-
-			if (Input.GetKeyDown("j")) {
-				if (jump > 0)
-					rB.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-			}
-
-			if (Input.GetKeyDown("n")) {
-				rB.AddForce(transform.forward * nitroForce, ForceMode.Impulse);
-			}
-
-//			Vector3 relativeVelocity = transform.InverseTransformDirection(rB.velocity);
-//			print(rB.velocity);
-//			float restFactor = 0.2f;
-//			if (Mathf.Abs(relativeVelocity.x) < restFactor)
-//				rB.velocity = Vector3.left * 0;
-//			if (Mathf.Abs(relativeVelocity.y) < restFactor)
-//				rB.velocity = Vector3.up * 0;
-//			if (Mathf.Abs(relativeVelocity.z) < restFactor)
-//				rB.velocity = Vector3.right * 0;
 		}
 	}
 }
