56a63751a91f8dabae25807acfbbc0c2df78c221
Riley Davidson <riley.max32@gmail.com>
2015-12-28 20:10:36 -0500

    Added timer and end game sound

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour {
+
+    public Text timerText;
+    public int currentTime = 10;
+    private AudioSource source;
+    public AudioClip endRound_Sound;
+
+    // Use this for initialization
+    void Start()
+    {
+        setTime();
+        InvokeRepeating("decreaseTimeLeft", 1.0f, 1.0f);
+
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+        
+    }
+
+  void decreaseTimeLeft()
+    {
+        currentTime--;
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            setTime();
+            source.PlayOneShot(endRound_Sound);
+            CancelInvoke("decreaseTimeLeft");
+        }
+
+        setTime();
+
+    }
+
+    void setTime()
+    {
+        timerText.text = "Time Left :" + currentTime.ToString();
+    }
+
+}
+
