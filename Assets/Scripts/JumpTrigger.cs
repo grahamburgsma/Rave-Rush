f0f296e3554c19ffb789660e95bd28b766181454
Graham Burgsma <graham.burgsma@gmail.com>
2016-01-02 12:59:51 -0500

    Added triggers for the obstacles

---
+++ Assets/Scripts/JumpTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class JumpTrigger : MonoBehaviour {
+
+	[SerializeField] Rigidbody car;
+
+	void Start() {
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Car")
+			car.AddForce(Vector3.up * 70000, ForceMode.Impulse);
+	}
+}
---
+++ Assets/Scripts/SpinTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpinTrigger : MonoBehaviour {
+
+	[SerializeField] Rigidbody car;
+	private int torqueCount = 0;
+	private int randomDirection = 1;
+
+	void Start() {
+	}
+
+	void Update() {
+		if (torqueCount > 0) {
+			car.AddTorque(Vector3.up * 800000000 * randomDirection, ForceMode.Acceleration);
+			torqueCount--;
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Car") {
+			System.Random r = new System.Random();
+			randomDirection = r.Next(0, 2) == 0 ? 1 : -1;
+			torqueCount = 50;
+		}
+	}
+}
