73a89a21843d6e60f32a44f62fa17852a834e1c0
Graham Burgsma <graham.burgsma@gmail.com>
2016-01-08 18:16:37 -0500

    New Menu - Converted to real buttons and callbacks for the UI and added car to the scene

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour {
+   
+	void Start() {
+        
+	}
+
+	public void onQuitGame() {
+		Application.Quit();
+	}
+
+	public void onTimeClick(int time) {
+		PlayerPrefs.SetInt("GameLength", time);
+		SceneManager.LoadScene(1);
+	}
+}
