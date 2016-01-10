f7ce464056674d6aeb1d3600a5aeaf6da34e2de0
Riley Davidson <riley.max32@gmail.com>
2016-01-09 22:02:02 -0500

    -Changed disco a bit, lights now spin

---
+++ Assets/Scripts/DiscoBallHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiscoBallHandler : MonoBehaviour {
+
+    [SerializeField]
+    GameObject discoball;
+
+
+    private Transform discoballTransform;
+	// Use this for initialization
+	void Start () {
+        discoballTransform = discoball.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    int i = 0;
+	void Update () {
+
+        discoballTransform.Rotate(0, 60f * Time.deltaTime, 0, Space.Self);
+        i++;
+        if(i >= 360)
+        {
+            i = 0;
+        }
+    }
+}
